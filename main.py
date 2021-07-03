ff66f889aa911132e31704ce937fff073f92b2b4
JeonHyeongJunKW <9965509@naver.com>
2021-07-03 16:30:19 +0900

    add simulate setting

--- main.py
+++ main.py
@@ -4,33 +4,12 @@ from PyQt5 import uic
 from PyQt5.QtGui import *
 import urllib.request
 import json
+
 form_class = uic.loadUiType("./main_window.ui")[0]
 from PyQt5.QtCore import Qt
 from Map import *
+from SimSet import *
 import os
-# class Widget4(QWidget):
-#
-#     def __init__(self, parent=None):
-#         QWidget.__init__(self, parent=parent)
-#         self.setWindowTitle("TSP 시뮬레이터")
-#         self.a = 0
-#         self.move(500,300)
-#         self.setFixedWidth(263)
-#         self.setFixedHeight(399)
-#         # self.show()
-#     def paintEvent(self, e):
-#         qp = QPainter()
-#         qp.begin(self)
-#         self.draw_line(qp)
-#         qp.end()
-#
-#     def draw_line(self,qp):
-#         qp.setPen(QPen(Qt.blue,8))
-#         qp.drawLine(30,230, 200, 50)
-#         qp.setPen(QPen(Qt.green, 12))
-#         qp.drawLine(140,60, 320, 280)
-#         qp.setPen(QPen(Qt.red,16))
-#         qp.drawLine(330, 250, 40, 190)
 
 
 class WindowClass(QMainWindow, form_class):
@@ -44,9 +23,13 @@ class WindowClass(QMainWindow, form_class):
         self.setFixedHeight(399)
         self.mapMaker = Widget_Warehouse()
         self.mapMaker.hide()
-        # self.setCentralWidget(self.test_widget)
+        self.simulation_maker =  Widget_SimSet()
+        self.simulation_maker.hide()
+
+        #시그널링 설정
         self.button_openmapmaker.clicked.connect(self.openMap)
         self.button_loadMap.clicked.connect(self.loadMap)
+        self.button_paramset.clicked.connect(self.setSim)
 
     def openMap(self):
         if self.mapMaker :
@@ -73,7 +56,29 @@ class WindowClass(QMainWindow, form_class):
                 self.saved_pk_point = data['pack_point']
                 self.saved_sp_point = data['start_point']
 
-
+    def setSim(self):
+        if self.simulation_maker.is_safe_close:
+            self.simulation_maker.show()
+            self.simulation_maker.is_safe_close= False
+            self.last_is_randomorder = self.simulation_maker.is_randomOrder
+            self.last_init_order = int(self.simulation_maker.lineEdit_initorder.text())
+            self.last_order_rate = int(self.simulation_maker.lineEdit_orderrate.text())
+            self.last_robot_cap  = int(self.simulation_maker.lineEdit_robotcap.text())
+            self.last_robot_number = int(self.simulation_maker.lineEdit_robotnumber.text())
+        else :
+            if self.last_is_randomorder:
+                self.simulation_maker.radioButton_orderrandom.setChecked(True)  # 랜덤으로 아이템을 배치하는 의미로 라디오버튼을 킵니다.
+                self.simulation_maker.lineEdit_initorder.setDisabled(True)
+                self.simulation_maker.lineEdit_orderrate.setDisabled(True)
+            else:
+                self.simulation_maker.radioButton_orderdirect.setChecked(True)  # 랜덤으로 아이템을 배치하는 의미로 라디오버튼을 킵니다.
+                self.simulation_maker.lineEdit_initorder.setEnabled(True)
+                self.simulation_maker.lineEdit_orderrate.setEnabled(True)
+            self.simulation_maker.lineEdit_initorder.setText(str(self.last_init_order))
+            self.simulation_maker.lineEdit_orderrate.setText(str(self.last_order_rate))
+            self.simulation_maker.lineEdit_robotcap.setText(str(self.last_robot_cap))
+            self.simulation_maker.lineEdit_robotnumber.setText(str(self.last_robot_number))
+            self.simulation_maker.show()
 if __name__ == "__main__":
     os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] ='1'
     app = QApplication(sys.argv)
